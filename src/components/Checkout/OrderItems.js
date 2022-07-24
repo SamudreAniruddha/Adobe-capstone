fb361f29317ebd5b71565e0421c838a5df97b8f8
samudreaniruddha <samudreaniruddha@gmail.com>
2022-07-24 13:14:33 +0530

    first commit

---
+++ src/components/Checkout/OrderItems.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useState } from "react";
+import './OrderItems.scss';
+import { useSelector } from "react-redux";
+
+function OrderItems() {
+    const [count, setCount] = useState(0);
+    const cartItems = useSelector(state => state.products.cart);
+
+         
+
+    return (
+    <>                  
+                       
+                        <div className='placed--items'> 
+                          <section>     
+                          <div className='order--items'>{cartItems.length} items in your order</div>
+                            <> 
+                            <div className="order--item" >
+                                {cartItems.map((product) => ( 
+                                <div className="order--item__box" role="Added Cart items" key={product.id}>
+                                    <img src={product.image} alt="Product Image" />
+                                    <div className="bag-product-details" role="Added Cart item Details">
+                                        <span className="bag-p-name">{product.title}</span>
+                                        <span className="bag-p-size">Size: -</span>
+                                        <span className="bag-p-color">Color: -</span>
+                                        <span className="bag-p-price">Price: ${product.price}</span>
+                                    </div>
+                                  </div>
+                                  ))}
+                            </div>
+                            </>
+                        </section>
+                        </div>
+                        {/* Added Cart items list end*/}
+                  
+    </>
+    )
+}
+
+export default OrderItems;
+
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import { Provider } from 'react-redux';
+import Router from './Router';
+import { store } from './store';
+
+import './aem-grid.css';
+
+ReactDOM.createRoot(
+  document.getElementById('root')
+  ).render(
+    <Provider store={store}>
+     
+      <Router />
+    </Provider>
+);
+
